bf41a0678729fba9afe27a3d9987d3e8c50e55e4
DamianPayano <payanodamian18@gmail.com>
2023-06-25 08:19:29 -0400

    Read Pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import OPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
+import wiringpi
 
-GPIO.setup(2, GPIO.OUT)
-print("done")
-
+# One of the following MUST be called before using IO functions:
+wiringpi.wiringPiSetup()      # For sequential pin numbering
+wiringpi.pinMode(2, 0)  
+wiringpi.digitalRead(2)   
